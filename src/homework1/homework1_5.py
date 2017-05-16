8b4cee34c4f4ed3c8619ca53b66f4a44eade4dd1
Chen Juan <e0253722@u.nus.edu>
2017-05-16 06:43:47 +0000

    Add first part of homework 1 problem 5.

---
+++ src/homework1/homework1_5.py
@@ -0,0 +1,25 @@
+import numpy as np
+import numpy.linalg as linalg
+import pandas as pd
+
+
+def linear_regression(X, y):
+    return linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+
+def main():
+    data = np.loadtxt('quasar_train.csv', delimiter=',')
+    wavelengths = data[0]
+    fluxes = data[1]
+    ones = np.ones(fluxes.size)
+
+    df_ones = pd.DataFrame(ones, columns=['xint'])
+    df_wavelengths = pd.DataFrame(wavelengths, columns=['wavelength'])
+    df_fluxes = pd.DataFrame(fluxes, columns=['flux'])
+
+    df = pd.concat([df_ones, df_wavelengths, df_fluxes], axis=1)
+    X = pd.concat([df['xint'], df['wavelength']], axis=1)
+    y = df['flux']
+
+    theta = linear_regression(X, y)
+
+    print('theta = {}'.format(theta))
