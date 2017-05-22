bda0122855242224f096b9b221ade408fcc52d49
Chen Juan <e0253722@u.nus.edu>
2017-05-22 22:39:48 +0000

    Correct find_best_threshold function.

--- src/homework2/find_best_threshold.py
+++ src/homework2/find_best_threshold.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 
 def find_best_threshold(X, y, p_dist):
@@ -11,7 +12,7 @@ def find_best_threshold(X, y, p_dist):
         inds = np.argsort(X[:,j])[::-1]
         x_sort = X[inds, j]
         y_sort = y[inds]
-        p_sort = p_sort[inds]
+        p_sort = p_dist[inds]
 
         s = x_sort[0] + 1
         possible_thresholds = (x_sort + np.roll(x_sort, 1)) / 2
@@ -20,14 +21,19 @@ def find_best_threshold(X, y, p_dist):
         increments = np.roll(p_sort * y_sort, 1)
         increments[0] = 0
 
-        empirical_errors = np.ones((rows, 1)).dot(p_sort.T.dot(np.where(y_sort == 1)))
+        empirical_errors = np.ones((rows, 1)).T.dot((p_sort.T * (y_sort == 1)))
+        print("SHAPE: {}".format(empirical_errors.shape), file = sys.stderr)
         empirical_errors = empirical_errors - np.cumsum(increments)
+        print("SHAPE: {}".format(empirical_errors.shape), file = sys.stderr)
 
-        thesh_index = np.argmin(empirical_errors)
+        thresh_index = np.argmin(empirical_errors)
         best_low = empirical_errors[thresh_index]
 
         thresh_high = np.argmax(empirical_errors)
         best_high = empirical_errors[thresh_high]
+
+        
+        print("HIGH: {}".format(best_high), file=sys.stderr)
 
         best_high = 1 - best_high
         best_err_j = min(best_high, best_low)
@@ -37,7 +43,7 @@ def find_best_threshold(X, y, p_dist):
 
         if best_err_j < best_err:
             ind = j
-            thresh = possible_thresholds[thresh_ind]
+            thresh = possible_thresholds[thresh_index]
             best_err = best_err_j
 
     return (ind, thresh)
