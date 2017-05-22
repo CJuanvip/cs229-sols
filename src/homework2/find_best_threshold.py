f2e470ddec528f54499439f05c3593d53f49963d
Chen Juan <e0253722@u.nus.edu>
2017-05-22 22:48:50 +0000

    Add documentation for find_best_threshold.

--- src/homework2/find_best_threshold.py
+++ src/homework2/find_best_threshold.py
@@ -1,8 +1,33 @@
 import numpy as np
-import sys
 
 
 def find_best_threshold(X, y, p_dist):
+    """
+    Finds the best threshold for the given data.
+
+    The function ``find_best_threshold`` returns a threshold
+    ``thresh`` and index ``ind`` that gives the best thresholded classifier for the
+    weights ``p_dist`` on the training data. That is, the returned index ``ind``
+    and threshold ``thresh`` minimize
+
+     ``sum_{i = 1}^m p(i) * 1{sign(X(i, ind) - thresh) ~= y(i)}``
+
+    OR
+
+     ``sum_{i = 1}^m p(i) * 1{sign(thresh - X(i, ind)) ~= y(i)}``.
+
+    We must check both signed directions, as it is possible that the best
+    decision stump (coordinate threshold classifier) is of the form
+    ``sign(threshold - x_j)`` rather than ``sign(x_j - threshold)``.
+
+    The data matrix ``X`` is of size m-by-n, where m is the training set size
+    and n is the dimension.
+
+    The implementation uses efficient sorting and data structures to perform
+    this calculation in time ``O(n m log(m))``, where the size of the data matrix
+    ``X`` is m-by-n.
+    """
+
     rows, cols = X.shape
     ind = 0
     thresh = 0
@@ -22,18 +47,13 @@ def find_best_threshold(X, y, p_dist):
         increments[0] = 0
 
         empirical_errors = np.ones((rows, 1)).T.dot((p_sort.T * (y_sort == 1)))
-        print("SHAPE: {}".format(empirical_errors.shape), file = sys.stderr)
         empirical_errors = empirical_errors - np.cumsum(increments)
-        print("SHAPE: {}".format(empirical_errors.shape), file = sys.stderr)
 
         thresh_index = np.argmin(empirical_errors)
         best_low = empirical_errors[thresh_index]
 
         thresh_high = np.argmax(empirical_errors)
         best_high = empirical_errors[thresh_high]
-
-        
-        print("HIGH: {}".format(best_high), file=sys.stderr)
 
         best_high = 1 - best_high
         best_err_j = min(best_high, best_low)
