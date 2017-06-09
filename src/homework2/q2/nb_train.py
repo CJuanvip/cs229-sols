33329293ae6a10fa260610466cdb51bcff5b4101
Chen Juan <e0253722@u.nus.edu>
2017-06-09 04:43:18 +0000

    Minor refactoring of Naive Bayes code.

--- src/homework2/q2/nb_train.py
+++ src/homework2/q2/nb_train.py
@@ -21,8 +21,8 @@ class NaiveBayes:
 
         dfp = np.zeros((2, word_count))
         # Compute the probability that a training email is spam.
-        phi_0 = np.sum(y == 0) / m
-        phi_1 = np.sum(y == 1) / m
+        prior_0 = np.sum(y == 0) / m
+        prior_1 = np.sum(y == 1) / m
 
         # Compute the conditional probabilities.
         for k in range(word_count):
@@ -33,8 +33,8 @@ class NaiveBayes:
             dfp[0, k] = phi_k_0
             dfp[1, k] = phi_k_1
 
-        self.phi_0 = phi_0
-        self.phi_1 = phi_1
+        self.prior_0 = prior_0
+        self.prior_1 = prior_1
         self.dfp = dfp
         self.tokenlist = tokenlist
 
@@ -43,8 +43,9 @@ class NaiveBayes:
         """
         Given a preprocessed email, classify it as SPAM or NOT SPAM.
         """
-        prob_y_equals_0 = self.phi_0
-        prob_y_equals_1 = self.phi_1
+        # TODO: Convert probabilities to logarithms to prevent numerical underflows.
+        prob_y_equals_0 = self.prior_0
+        prob_y_equals_1 = self.prior_1
 
         probs_x_given_0 = self.dfp[0, np.where(x != 0)]
         probs_x_given_0 = probs_x_given_0.reshape(probs_x_given_0.shape[1])
@@ -54,8 +55,6 @@ class NaiveBayes:
         probs_0 = np.power(prob_y_equals_0 * probs_x_given_0, x[x != 0])
         probs_1 = np.power(prob_y_equals_1 * probs_x_given_1, x[x != 0])
 
-        # We call index zero because we are dealingn with a pandas DataFrame.
-        # TODO: Remove the pandas dataframes from the ML code and wrap it instead.
         numer_0 = np.product(probs_0)
         numer_1 = np.product(probs_1)
         denom = numer_0 + numer_1
@@ -67,3 +66,15 @@ class NaiveBayes:
         py_given_x[0] = numer_0 / denom
         
         return np.argmax(py_given_x)
+
+
+    def to_dataframe(self):
+        df = pd.DataFrame(np.zeros((self.dfp.shape[0], self.dfp.shape[1]+1)), columns=self.tokenlist)
+        df.iloc[0,0] = self.prior_0
+        df.iloc[1,0] = self.prior_1
+        df.iloc[:, 1:] = self.dfp
+
+        return df
+
+    def __str__(self):
+        return str(self.to_dataframe())
